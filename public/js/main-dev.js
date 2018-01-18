8ed27cd736d3540406b350f6e67f28475f33f174
goldminer1030 <goldminer1030@gmail.com>
2018-01-18 19:21:45 +0100

    alpha version

--- public/js/main-dev.js
+++ public/js/main-dev.js
@@ -20,6 +20,26 @@ $(document).ready(function () {
     });
   }
 
+  if ($("#gmap-contact").length > 0) {
+    var myLatlng = new google.maps.LatLng(43.5571948, 2.1153791);
+    var myOptions = {
+      zoom: 16,
+      center: myLatlng,
+      disableDefaultUI: true,
+      navigationControl: false,
+
+      mapTypeControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById("gmap-contact"), myOptions);
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: "7 Route de Soulet 81580 Soual, france"
+    });
+  }
+
   // owl-carousel
   if ($(".owl-carousel").length > 0) {
     var owl = $(".owl-carousel");
